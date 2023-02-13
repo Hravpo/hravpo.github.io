46c775d321d1dffc9508cf61e6a2ab6ee80076f1
Maksim <maks.glagckih@gmail.com>
2023-02-13 14:12:35 +0500

    something adds

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+var name = "Ivan";
+
+let number = 7;
+const pi = 3.14;
+
+number = 4;
+
+let leftBorderWidth = 200;
+
+if (2*6 == 8*1) {
+    console.log ('Верно')
+} else {
+    console.log ('Ошибка')
+}
