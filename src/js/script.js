4c4b9bb5c310f10540a6217c475b4808949abc12
Maks <maks.glagckih@gmail.com>
2023-02-17 09:04:44 +0500

    tabs modification

--- src/js/script.js
+++ src/js/script.js
@@ -54,9 +54,14 @@ logging(3,6); */
                 }
             }
         ]
+      }); */
+    $(document).ready(function(){
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
       });
-  });
- */
+  
 
   const slider = tns({
     container: '.carousel__inner',
@@ -75,3 +80,4 @@ logging(3,6); */
     slider.goTo('next');
   }); 
 
+});
