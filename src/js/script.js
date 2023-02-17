5571706105a6b422df5b2348972216fc509ffa98
Maks <maks.glagckih@gmail.com>
2023-02-17 12:46:18 +0500

    end of catalog mod

--- src/js/script.js
+++ src/js/script.js
@@ -56,13 +56,35 @@ logging(3,6); */
         ]
       }); */
     $(document).ready(function(){
+
       $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
           .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
       });
   
-  const slider = tns({
+      $('.catalog-item__link').each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+      })
+      $('.catalog-item__back').each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+      })
+});
+
+
+
+
+
+
+const slider = tns({
     container: '.carousel__inner',
     items: 1,
     slideBy: 'page',
@@ -78,20 +100,3 @@ logging(3,6); */
   document.querySelector('.next').addEventListener ('click', function () {
     slider.goTo('next');
   }); 
-
-    $('.catalog-item__link').each(function(i){
-        $(this).on('click', function(e){
-            e.preventDefault();
-            $('.catalog-item__content').eq($(i)).toggleClass('catalog-item__content_active');
-            $('.catalog-item__list').eq($(i)).toggleClass('.catalog-item__list_active');
-        })
-    })
-
-    $('.catalog-item__back').each(function(i){
-        $(this).on('click', function(e){
-            e.preventDefault();
-            $('.catalog-item__content').eq($(i)).toggleClass('catalog-item__content_active');
-            $('.catalog-item__list').eq($(i)).toggleClass('.catalog-item__list_active');
-        })
-    })
-});
