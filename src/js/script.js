588cac073582972cb1de87202cd8b8f20eff883a
Maksim <maks.glagckih@gmail.com>
2023-02-22 09:55:58 +0500

    Whole website mod

--- src/js/script.js
+++ src/js/script.js
@@ -77,6 +77,54 @@ logging(3,6); */
             $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
         })
       })
+
+    //Modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i) {
+       $(this).on('click', function() {
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text())
+        $('.overlay, #order').fadeIn('slow');
+      }); 
+    });
+
+    function validateForms(form){
+        $(form).validate({
+          rules: {
+              name:{
+                  required: true,
+                  minlength: 2
+                },
+              phone: "required",
+              email: {
+                  required: true,
+                  email:true
+              }
+          },
+          messages: {
+              name: {
+                  required: "Пожалуйста, введите свое имя",
+                  minlength: jQuery.validator.format("Введите {0} символов")
+                },
+              phone: "Пожалуйста введите свой номер телефона",
+              email: {
+                required: "Пожалуйста, введите свою почту",
+                email: "Неправильно введен почтовый адрес"
+              }
+            }
+      });
+    };
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+7(999) 999-9999");
 });
 
 
@@ -99,4 +147,5 @@ const slider = tns({
 
   document.querySelector('.next').addEventListener ('click', function () {
     slider.goTo('next');
-  }); 
+
+}); 
