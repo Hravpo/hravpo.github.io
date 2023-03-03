e32cef774185e82f620851e1d0d1013e3b28b950
Maksim <maks.glagckih@gmail.com>
2023-03-03 09:52:53 +0500

    animation add

--- src/js/script.js
+++ src/js/script.js
@@ -165,7 +165,7 @@ logging(3,6); */
     }
   }); 
 
-  $("a[href^='#']").click(function(){
+  $("a[href=#up]").click(function(){
     const _href = $(this).attr("href");
     $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
     return false;
